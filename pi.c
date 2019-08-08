436941daa0f49e3232350e9d0047bc8192ed07c7
raintre <33364187+raintre@users.noreply.github.com>
2019-08-08 17:15:22 +0900

    Add files via upload

---
+++ pi.c
@@ -0,0 +1,28 @@
+    #include<stdio.h>
+    #include<stdlib.h>
+
+    double pi_compute(double n);
+
+    double pi_compute(double n)
+    {
+
+        if(n == 0.0) {
+            return 2.0;
+        }
+        else {
+            return(n/(2.0*n + 1.0)*pi_compute(n - 1.0));
+        }
+    }
+
+    int main(void)
+    {
+        double sum = 0.0, n;
+        printf("Enter the number of terms!\n");
+        scanf("%lf", &n);
+        for(double i = 0.0; i < n; i += 1.0){
+            sum += pi_compute(i);
+        }
+
+        printf("%f\n", sum);
+        return EXIT_SUCCESS;
+    }
